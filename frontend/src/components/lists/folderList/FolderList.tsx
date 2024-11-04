edb3b8d63f7de0e0dbd1227f348ff78e2c42d0f0
raparisonbryan <raparisonbryan@yahoo.fr>
2024-11-04 11:35:29 +0100

    fix: packages

---
+++ frontend/src/components/lists/folderList/FolderList.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '@/components/lists/folderList/folderList.module.scss';
+
+const folders = ['Folder1', 'Folder2', 'Folder3'];
+
+const FolderList = ({ onSelectFolder }: { onSelectFolder: (folder: string) => void }) => {
+    return (
+        <div>
+            <p className={styles.title}>Folders</p>
+            <div className={styles.folders_list}>
+                {folders.map(folder => (
+                    <div className={styles.folders} key={folder} onClick={() => onSelectFolder(folder)}>
+                        {folder}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default FolderList;
