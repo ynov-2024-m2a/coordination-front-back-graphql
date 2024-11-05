336d510bbf262a179b6cbe47b4e61a5c1cfcd1b4
raparisonbryan <raparisonbryan@yahoo.fr>
2024-11-05 15:20:05 +0100

    feat: apollo graphql api

--- frontend/src/components/lists/folderList/FolderList.tsx
+++ frontend/src/components/lists/folderList/FolderList.tsx
@@ -6,7 +6,7 @@ const folders = ['Folder1', 'Folder2', 'Folder3'];
 const FolderList = ({ onSelectFolder }: { onSelectFolder: (folder: string) => void }) => {
     return (
         <div>
-            <p className={styles.title}>Folders</p>
+            <p className={styles.title}>Dossiers</p>
             <div className={styles.folders_list}>
                 {folders.map(folder => (
                     <div className={styles.folders} key={folder} onClick={() => onSelectFolder(folder)}>
