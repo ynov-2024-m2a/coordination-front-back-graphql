2cb743c586c0d60a68446b4b2bb3e0df36eaf948
raparisonbryan <raparisonbryan@yahoo.fr>
2024-11-06 14:21:21 +0100

    feat: display file in folders

--- frontend/src/components/lists/fileList/FileList.tsx
+++ frontend/src/components/lists/fileList/FileList.tsx
@@ -3,28 +3,31 @@ import styles from "./fileList.module.scss";
 import { useQuery, gql } from '@apollo/client';
 
 const GET_FILES = gql`
-    query {
-      root {
+  query {
+    root {
+      folders {
+        name
         files {
-          name      
+          name
         }
       }
     }
+  }
 `;
 
 const FileList = ({ folder, onSelectFile }: { folder: string, onSelectFile: (file: string) => void }) => {
-    const { loading, error, data } = useQuery(GET_FILES, {
-        variables: { folder },
-    });
+    const { loading, error, data } = useQuery(GET_FILES);
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
+
+    const selectedFolder = data.root.folders.find((f: { name: string }) => f.name === folder);
 
     return (
         <div>
             <p className={styles.title}>{folder}</p>
             <div className={styles.files_list}>
-                {data.root.files.map((file: { name: string }) => (
+                {selectedFolder.files.map((file: { name: string }) => (
                     <div className={styles.files} key={file.name} onClick={() => onSelectFile(file.name)}>
                         {file.name}
                     </div>
